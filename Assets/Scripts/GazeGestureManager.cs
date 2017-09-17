71ade14bd9bef618598a2cc52fdd82e669b8044c
unknown <fudong@umich.edu>
2017-09-17 12:11:37 -0400

    Initial commit

---
+++ Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine.VR.WSA.WebCam;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+public class GazeGestureManager : MonoBehaviour
+{
+	public static GazeGestureManager Instance { get; private set; }
+
+    GestureRecognizer recognizer;
+
+	Resolution cameraResolution;
+
+	PhotoCapture photoCaptureObject = null;
+
+	Vector3 cameraPosition;
+	Quaternion cameraRotation;
+
+	bool _busy = false;
+
+	public GameObject textPrefab;
+	public GameObject status;
+	public GameObject framePrefab;
+
+	public string faceApiUrl = "http://faceapi.us.to/";
+    const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+    const string uriBase2 = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify";
+    public static string faceID;
+    public static string verifyResponse;
+    public static string personID;
+    public static string confidence;
+
+    void OnPhotoCaptureCreated(PhotoCapture captureObject)
+	{
+		photoCaptureObject = captureObject;
+
+		cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+
+		CameraParameters c = new CameraParameters();
+		c.hologramOpacity = 0.0f;
+		c.cameraResolutionWidth = cameraResolution.width;
+		c.cameraResolutionHeight = cameraResolution.height;
+		c.pixelFormat = CapturePixelFormat.PNG;
+
+		captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
+	}
+
+	private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+	{
+		if (result.success)
+		{
+			Debug.Log("Camera ready");
+			photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+		}
+		else
+		{
+			Debug.LogError("Unable to start photo mode!");
+			_busy = false;
+		}
+	}
+    /// <summary>
+    /// Gets the analysis of the specified image file by using the Computer Vision REST API.
+    /// </summary>
+    /// <param name="imageFilePath">The image file.</param>
+    static async void MakeAnalysisRequest(byte[] imageData)
+    {
+        HttpClient client = new HttpClient();
+
+        // Request headers.
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "b7f95616881141269d7daa80123a15c4");
+
+        // Request parameters. A third optional parameter is "details".
+        string requestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+        // Assemble the URI for the REST API Call.
+        string uri = uriBase + "?" + requestParameters;
+
+        HttpResponseMessage response;
+
+
+        using (ByteArrayContent content = new ByteArrayContent(imageData))
+        {
+            // This example uses content type "application/octet-stream".
+            // The other content types you can use are "application/json" and "multipart/form-data".
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            // Execute the REST API call.
+            response = await client.PostAsync(uri, content);
+
+            // Get the JSON response.
+            faceID = await response.Content.ReadAsStringAsync(); // string
+        }
+    }
+    static async void MakeVerifyRequest()
+    {
+        HttpClient client = new HttpClient();
+
+        // Request headers.
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "b7f95616881141269d7daa80123a15c4");
+        // Assemble the URI for the REST API Call.
+        string uri2 = uriBase2;
+
+        HttpResponseMessage response;
+
+        JSONObject j = new JSONObject(faceID);
+
+        personID = j["faceId"].ToString();
+
+        string chrisID = "ab25e0a1-1334-4b55-848c-5371519b64e3";
+
+        byte[] byteData = Encoding.UTF8.GetBytes("{faceId1:" + personID + ", faceId2:" + chrisID + ",}" );
+
+        using (var content = new ByteArrayContent(byteData))
+        {
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            response = await client.PostAsync(uri2, content);
+
+            verifyResponse = await response.Content.ReadAsStringAsync(); // string
+
+        }
+
+        JSONObject verifiedJSON = new JSONObject(verifyResponse);
+
+        confidence = verifiedJSON["confidence"].ToString();
+
+    }
+    
+    IEnumerator<object> PostToFaceAPI(byte[] imageData, Matrix4x4 cameraToWorldMatrix, Matrix4x4 pixelToCameraMatrix) {
+
+        MakeAnalysisRequest(imageData);
+        MakeVerifyRequest();
+
+        WWW www = new WWW(faceApiUrl, imageData);
+        yield return www;
+        string responseString = www.text;
+        Debug.Log(responseString);
+        JSONObject j = new JSONObject(responseString);
+        var existing = GameObject.FindGameObjectsWithTag("faceText");
+
+        foreach (var go in existing)
+        {
+            Destroy(go);
+        }
+
+        existing = GameObject.FindGameObjectsWithTag("faceBounds");
+
+        foreach (var go in existing)
+        {
+            Destroy(go);
+        }
+
+        if (j.list.Count == 0)
+        {
+            status.GetComponent<TextMesh>().text = "no faces found";
+            yield break;
+        }
+        else
+        {
+            status.SetActive(false);
+        }
+
+        foreach (var result in j.list)
+        {
+            GameObject txtObject = (GameObject)Instantiate(textPrefab);
+            TextMesh txtMesh = txtObject.GetComponent<TextMesh>();
+            var r = result["kairos"];
+            float top = -(r["topLeftY"].f / cameraResolution.height - .5f);
+            float left = r["topLeftX"].f / cameraResolution.width - .5f;
+            float width = r["width"].f / cameraResolution.width;
+            float height = r["height"].f / cameraResolution.height;
+
+            GameObject faceBounds = (GameObject)Instantiate(framePrefab);
+            faceBounds.transform.position = cameraToWorldMatrix.MultiplyPoint3x4(pixelToCameraMatrix.MultiplyPoint3x4(new Vector3(left + width / 2, top, 0)));
+            faceBounds.transform.rotation = cameraRotation;
+            Vector3 scale = pixelToCameraMatrix.MultiplyPoint3x4(new Vector3(width, height, 0));
+            scale.z = .1f;
+            faceBounds.transform.localScale = scale;
+            faceBounds.tag = "faceBounds";
+            Debug.Log(string.Format("{0},{1} translates to {2},{3}", left, top, faceBounds.transform.position.x, faceBounds.transform.position.y));
+
+            Vector3 origin = cameraToWorldMatrix.MultiplyPoint3x4(pixelToCameraMatrix.MultiplyPoint3x4(new Vector3(left + width + .1f, top, 0)));
+            txtObject.transform.position = origin;
+            txtObject.transform.rotation = cameraRotation;
+            txtObject.tag = "faceText";
+            if (j.list.Count > 1)
+            {
+                txtObject.transform.localScale /= 2;
+            }
+
+            //txtMesh.text = "Name: Chris Hoang\n Age: 19\n School: University of Michigan\n Interests: Tennis, Piano, Mr. Robot\n";
+            txtMesh.text = faceID;
+        }
+}
+
+    
+
+	void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+	{
+		if (result.success)
+		{
+			Debug.Log("photo captured");
+			List<byte> imageBufferList = new List<byte>();
+			// Copy the raw IMFMediaBuffer data into our empty byte list.
+			photoCaptureFrame.CopyRawImageDataIntoBuffer(imageBufferList);
+
+			var cameraToWorldMatrix = new Matrix4x4();
+			photoCaptureFrame.TryGetCameraToWorldMatrix(out cameraToWorldMatrix);
+			
+			cameraPosition = cameraToWorldMatrix.MultiplyPoint3x4(new Vector3(0,0,-1));
+			cameraRotation = Quaternion.LookRotation(-cameraToWorldMatrix.GetColumn(2), cameraToWorldMatrix.GetColumn(1));
+
+			Matrix4x4 projectionMatrix;
+			photoCaptureFrame.TryGetProjectionMatrix(Camera.main.nearClipPlane, Camera.main.farClipPlane, out projectionMatrix);
+			Matrix4x4 pixelToCameraMatrix = projectionMatrix.inverse;
+
+			status.GetComponent<TextMesh>().text = "photo captured, processing...";
+			status.transform.position = cameraPosition;
+			status.transform.rotation = cameraRotation;
+
+            //MakeAnalysisRequest(imageBufferList.ToArray());
+			StartCoroutine(PostToFaceAPI(imageBufferList.ToArray(), cameraToWorldMatrix, pixelToCameraMatrix));
+			//StartCoroutine(PostToFaceAPI(System.IO.File.ReadAllBytes("nyou045.png"), cameraToWorldMatrix, pixelToCameraMatrix));
+		}
+		photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+	}
+
+	void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+	{
+		photoCaptureObject.Dispose();
+		photoCaptureObject = null;
+		_busy = false;
+	}
+
+	// Use this for initialization
+	void Awake()
+	{
+		Instance = this;
+
+		// Set up a GestureRecognizer to detect Select gestures.
+		recognizer = new GestureRecognizer();
+		recognizer.TappedEvent += (source, tapCount, ray) =>
+		{
+			Debug.Log("tap");
+			if (!_busy)
+			{
+				_busy = true;
+				status.GetComponent<TextMesh>().text = "taking photo...";
+				status.SetActive(true);
+				PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
+			} else
+			{
+				status.GetComponent<TextMesh>().text = "busy...";
+				status.SetActive(true);
+			}
+		};
+		recognizer.StartCapturingGestures();
+		status.GetComponent<TextMesh>().text = "taking photo...";
+		_busy = true;
+
+		PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
+	}
+}
+
+internal class Task<T>
+{
+}
---
+++ Assets/Scripts/JSON_test.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JSON_test : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		string json = @"[
+	{
+			""kairos"": {
+				""eyeDistance"": 16, 
+            ""topLeftX"": 29, 
+            ""topLeftY"": 30, 
+            ""chinTipX"": 45, 
+            ""width"": 36, 
+            ""yaw"": 7, 
+            ""chinTipY"": 68, 
+            ""confidence"": 0.9993, 
+            ""height"": 36, 
+            ""rightEyeCenterY"": 39, 
+            ""rightEyeCenterX"": 37, 
+            ""leftEyeCenterY"": 39, 
+            ""leftEyeCenterX"": 54, 
+            ""pitch"": -4, 
+            ""attributes"": {
+					""gender"": {
+						""femaleConfidence"": 0, 
+                    ""type"": ""M"", 
+                    ""maleConfidence"": 1
+	
+				}, 
+                ""age"": 27, 
+                ""hispanic"": 0.02015, 
+                ""lips"": ""Together"", 
+                ""other"": 0.00129, 
+                ""black"": 2e-05, 
+                ""asian"": 9e-05, 
+                ""white"": 0.97845, 
+                ""glasses"": ""None""
+
+			}, 
+            ""face_id"": 1, 
+            ""quality"": 0.8038, 
+            ""roll"": 0
+	
+		}, 
+        ""text"": ""Age: 27\nGender: M\nEthnicity: white: 98%, hispanic: 2%\nGlasses: None\nLips: Together\nEmotion: happiness: 78%\nSmile: 44%\nBeauty: F:56%, M:56%\nRecognition confidence: 93.96%\nUPI: nyou045\nName: Mr Nick Young\nPosition: Research IT Specialist\nDepartment: Computer Science\nReports to: Mr Marcus Gustafsson\n"", 
+        ""fpp"": {
+				""attributes"": {
+					""emotion"": {
+						""neutral"": 20.253, 
+                    ""sadness"": 0.673, 
+                    ""disgust"": 0.907, 
+                    ""anger"": 0.053, 
+                    ""surprise"": 0.3, 
+                    ""fear"": 0.053, 
+                    ""happiness"": 77.761
+	
+				}, 
+                ""beauty"": {
+						""female_score"": 56.236, 
+                    ""male_score"": 55.571
+
+				}, 
+                ""gender"": {
+						""value"": ""Male""
+
+				}, 
+                ""age"": {
+						""value"": 30
+
+				}, 
+                ""eyestatus"": {
+						""left_eye_status"": {
+							""normal_glass_eye_open"": 0.034, 
+                        ""no_glass_eye_close"": 0.0, 
+                        ""occlusion"": 0.0, 
+                        ""no_glass_eye_open"": 99.966, 
+                        ""normal_glass_eye_close"": 0.0, 
+                        ""dark_glasses"": 0.0
+	
+					}, 
+                    ""right_eye_status"": {
+							""normal_glass_eye_open"": 0.397, 
+                        ""no_glass_eye_close"": 0.0, 
+                        ""occlusion"": 0.0, 
+                        ""no_glass_eye_open"": 99.601, 
+                        ""normal_glass_eye_close"": 0.002, 
+                        ""dark_glasses"": 0.0
+
+					}
+					}, 
+                ""glass"": {
+						""value"": ""None""
+
+				}, 
+                ""headpose"": {
+						""yaw_angle"": 1.8853482, 
+                    ""pitch_angle"": 6.320396, 
+                    ""roll_angle"": -2.5211878
+
+				}, 
+                ""blur"": {
+						""blurness"": {
+							""threshold"": 50.0, 
+                        ""value"": 1.488
+	
+					}, 
+                    ""motionblur"": {
+							""threshold"": 50.0, 
+                        ""value"": 1.488
+
+					}, 
+                    ""gaussianblur"": {
+							""threshold"": 50.0, 
+                        ""value"": 1.488
+
+					}
+					}, 
+                ""smile"": {
+						""threshold"": 30.1, 
+                    ""value"": 44.069
+
+				}, 
+                ""facequality"": {
+						""threshold"": 70.1, 
+                    ""value"": 92.136
+
+				}, 
+                ""ethnicity"": {
+						""value"": ""White""
+
+				}
+				}, 
+            ""face_token"": ""f163a6c3c07461e71f5da1eab755ce36"", 
+            ""face_rectangle"": {
+					""width"": 37, 
+                ""top"": 32, 
+                ""height"": 37, 
+                ""left"": 27
+
+			}
+			}, 
+        ""of"": {
+				""confidence"": 0.9395938787156883, 
+            ""data"": {
+					""phoneNumbers"": [
+	                    {
+                        ""phone"": ""+64 9 923 9740"",
+	                        ""type"": ""ddi""
+
+					}, 
+                    {
+                        ""phone"": ""89740"", 
+                        ""type"": ""extension""
+                    }
+                ], 
+                ""orgUnits"": [
+                    ""COMSCI"",
+                    ""SCIFAC""
+                ], 
+                ""upi"": ""nyou045"", 
+                ""groupProfile"": false, 
+                ""positions"": [
+                    {
+                        ""department"": {
+                            ""name"": ""Computer Science"",
+                            ""unit"": ""COMSCI""
+                        }, 
+                        ""position"": ""Research IT Specialist"", 
+                        ""orgUnits"": [
+                            {
+                                ""name"": ""Faculty of Science"",
+                                ""unit"": ""SCIFAC""
+                            }
+                        ], 
+                        ""reportsTo"": {
+                            ""positionId"": 2, 
+                            ""url"": ""m-gustafsson"", 
+                            ""name"": ""Mr Marcus Gustafsson""
+                        }
+                    }
+                ], 
+                ""image"": ""/people/imageraw/nick-young/10547437/large"", 
+                ""profileUrl"": ""nick-young"", 
+                ""fullName"": ""Mr Nick Young"", 
+                ""emailAddresses"": [
+                    ""nick.young@auckland.ac.nz"",
+                    ""nyou045@aucklanduni.ac.nz""
+                ]
+            }, 
+            ""uid"": ""nyou045"", 
+            ""face_rectangle"": {
+                ""width"": 37, 
+                ""top"": 30, 
+                ""left"": 25, 
+                ""height"": 37
+            }
+        }
+    }
+]";
+		JSONObject j = new JSONObject(json);
+		Debug.Log(j[0]["text"].str);
+	}
+}
